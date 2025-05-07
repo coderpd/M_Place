30e45c76082016de479d0bbc2b6c72d6b9aa3338
coderpd <dhineshp1801@gmail.com>
2025-05-07 16:31:13 +0530

    Added Chunks

--- frontend/app/vendorDashboard/page.js
+++ frontend/app/vendorDashboard/page.js
@@ -1,10 +1,12 @@
 "use client";
 
 import ProductCards from "./productcards/page";
+import ChunkErrorHandler from "../Components/ChunkErrorHandler";
 
 export default function VendorDashboardPage() {
   return (
     <div>
+      <ChunkErrorHandler/>
    <ProductCards/>
     </div>
   );
