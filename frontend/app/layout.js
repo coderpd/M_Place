57578d8a4e62153f1a7e292ac40164fc266f0173
coderpd <dhineshp1801@gmail.com>
2025-05-07 16:48:19 +0530

    chunk is removed

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ChunkErrorHandler from "./Components/ChunkErrorHandler";
+
 
 
 const geistSans = Geist({
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ChunkErrorHandler /> 
+     
         {children}
       </body>
     </html>
--- frontend/app/vendorDashboard/page.js
+++ frontend/app/vendorDashboard/page.js
@@ -1,12 +1,11 @@
 "use client";
 
 import ProductCards from "./productcards/page";
-import ChunkErrorHandler from "../Components/ChunkErrorHandler";
 
 export default function VendorDashboardPage() {
   return (
     <div>
-      <ChunkErrorHandler/>
+      
    <ProductCards/>
     </div>
   );
