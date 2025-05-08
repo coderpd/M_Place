edda0528ea7d43e0ef684b85c577c06c18d89566
coderpd <dhineshp1801@gmail.com>
2025-05-08 10:10:28 +0530

    remove chunk

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Force a unique build ID on each deployment
-    generateBuildId: async () => {
-      return Date.now().toString();
-    },
-  
-    // Modify Webpack config to help with proper chunk naming and caching
-    webpack(config) {
-      config.output.filename = 'static/chunks/[name].[contenthash].js';
-      config.output.chunkFilename = 'static/chunks/[name].[contenthash].js';
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+
+const nextConfig = {};
+export default nextConfig;
