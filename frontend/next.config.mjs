176a74cd4cd43262ac1c01d83b4e6472d5e96213
coderpd <dhineshp1801@gmail.com>
2025-05-08 09:59:21 +0530

    added chunk loader

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Force a unique build ID on each deployment
+    generateBuildId: async () => {
+      return Date.now().toString();
+    },
+  
+    // Modify Webpack config to help with proper chunk naming and caching
+    webpack(config) {
+      config.output.filename = 'static/chunks/[name].[contenthash].js';
+      config.output.chunkFilename = 'static/chunks/[name].[contenthash].js';
+      return config;
+    },
+  };
+  
+  export default nextConfig;
